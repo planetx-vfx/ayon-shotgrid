64326e4dcc09a6d5ac3887a7e604e913987d35ad
Wilfried Pollan <wilfried.pollan@planetx.nl>
2025-10-01 12:33:00 +0200

    feat: Update version

--- package.py
+++ package.py
@@ -1,15 +1,17 @@
 name = "shotgrid"
 title = "Shotgrid"
-version = "0.6.10"
+_version = "0.6.10"
+_dev = "+dev2"
+version = _version + _dev
 client_dir = "ayon_shotgrid"
 
 services = {
     "ShotgridLeecher": {
-        "image": f"ynput/ayon-shotgrid-leecher:{version}"},
+        "image": f"ynput/ayon-shotgrid-leecher:{_version}"},
     "ShotgridProcessor": {
-        "image": f"ynput/ayon-shotgrid-processor:{version}"},
+        "image": f"ynput/ayon-shotgrid-processor:{_version}"},
     "ShotgridTransmitter": {
-        "image": f"ynput/ayon-shotgrid-transmitter:{version}"},
+        "image": f"ynput/ayon-shotgrid-transmitter:{_version}"},
 }
 ayon_required_addons = {
     "core": ">=0.3.0",
